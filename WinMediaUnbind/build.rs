cf118a881ab24bee5abb907a619fbde0ad6bf39f
NeetheCheeBao <96571725+NeetheCheeBao@users.noreply.github.com>
2026-05-03 17:43:39 +0800

    Add files via upload

---
+++ WinMediaUnbind/build.rs
@@ -0,0 +1,27 @@
+use std::io;
+
+#[cfg(windows)]
+fn main() -> io::Result<()> {
+    let mut res = winres::WindowsResource::new();
+    res.set_manifest(r#"
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+    <dependency>
+        <dependentAssembly>
+            <assemblyIdentity type="win32" name="Microsoft.Windows.Common-Controls" version="6.0.0.0" processorArchitecture="*" publicKeyToken="6595b64144ccf1df" language="*" />
+        </dependentAssembly>
+    </dependency>
+    <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+        <security>
+            <requestedPrivileges>
+                <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
+            </requestedPrivileges>
+        </security>
+    </trustInfo>
+</assembly>
+"#);
+    res.compile()
+}
+
+#[cfg(not(windows))]
+fn main() {}
